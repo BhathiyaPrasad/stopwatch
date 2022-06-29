9e6b617da3a44dc7bf0cf6dc125bf11fafe680ac
Bhathiya Prasad <bhathiyaprasad217@gmail.com>
2022-06-29 21:14:39 +0530

    format the code

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const timer = document.getElementById('stopwatch')
-var hr  = 0;
+var hr = 0;
 var min = 0;
 var sec = 0;
 var stoptime = true;
 
-function startTimer(){
-    if (stoptime== true) {
+function startTimer() {
+    if (stoptime == true) {
         stoptime = false;
         timerCycle();
 
@@ -18,40 +18,40 @@ function stopTimer() {
     }
 }
 
-function timerCycle(){
-    if (stoptime == false){
-        sec = parseint
+function timerCycle() {
+    if (stoptime == false) {
+        sec = parseInt
         min = parseInt(sec);
         hr = parseInt(hr);
-        
+
         sec = sec + 1;
 
-        if (sec == 60)
-        {
-            min=min + 1;
-            sec = 0;
-        }
-        if(min == 60){
-            hr = hr+1;
-            min = 0
-            sec = 0;
-        }
-        
-        if (sec < 10 || sec == 0) {
-            sec = '0' + sec;
-          }
-          if (min < 10 || min == 0) {
-            min = '0' + min;
-          }
-          if (hr < 10 || hr == 0) {
-            hr = '0' + hr;
-          }
+        if (sec == 60) {
+            {
+                min = min + 1;
+                sec = 0;
+            }
+            if (min == 60) {
+                hr = hr + 1;
+                min = 0
+                sec = 0;
+            }
 
-          timer.innerHTML= hr + ':' + sec;
+            if (sec < 10 || sec == 0) {
+                sec = '0' + sec;
+            }
+            if (min < 10 || min == 0) {
+                min = '0' + min;
+            }
+            if (hr < 10 || hr == 0) {
+                hr = '0' + hr;
+            }
 
-          setTimeout("timerCycle()",1000);
+            timer.innerHTML = hr + ':' + sec;
+
+            setTimeout("timerCycle()", 1000);
         }
-function resetTimer() {
-    timer.innerHTML = '00.00.00';
-}
-}
+
+        function resetTimer() {
+            timer.innerHTML = '00.00.00';
+        }
